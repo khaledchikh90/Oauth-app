f954fc9a2ac9016a2db93877a2e32798b8a167d1
khaledchikh90 <khaledchikh90@gmail.com>
2017-11-03 15:50:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const app = express();
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+// setup a view engine
+app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+  maxAge : 24 * 60 * 60 * 1000,
+  keys : [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log('connected to mongodb');
+});
+
+// setup routes
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
+// create home route
+app.get('/', (req, res) => {
+  res.render('home', {user : req.user});
+});
+
+app.listen(3000, () => {
+  console.log('app now is listening on port 3000');
+});
